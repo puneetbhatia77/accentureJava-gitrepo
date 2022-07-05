2633465ecb4522c875178caa381e0ee279104df1
puneetbhatia77 <puneetbhatia77@gmail.com>
2022-07-05 16:36:17 +0530

    Update AccentureFirstJavaProject.java

--- src/AccentureFirstJavaProject.java
+++ src/AccentureFirstJavaProject.java
@@ -3,7 +3,7 @@ public class AccentureFirstJavaProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        Syste.out.println("Welcome to Java to test polling");
+        System.out.println("Welcome to Java to test polling");
 	}
 
 }
